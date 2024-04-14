ac29f1d219e5c22a5a83f852845128d0e698af7e
devOlbap <pa.garridoc@duocuc.cl>
2024-04-13 21:52:34 -0400

    configuraciones de proyecto

---
+++ usuarios/src/test/java/com/users/usuarios/UsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.users.usuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
