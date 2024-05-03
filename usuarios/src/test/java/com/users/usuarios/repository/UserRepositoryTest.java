cceabce97cd12d909d88c35ebb1378cd48d47c97
devOlbap <pa.garridoc@duocuc.cl>
2024-05-02 23:41:45 -0400

    modificando Test de Repository

--- usuarios/src/test/java/com/users/usuarios/repository/UserRepositoryTest.java
+++ usuarios/src/test/java/com/users/usuarios/repository/UserRepositoryTest.java
@@ -2,9 +2,11 @@ package com.users.usuarios.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
-
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,16 +18,27 @@ import com.users.usuarios.model.Usuario;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UserRepositoryTest {
-    @Autowired
+    
+    @Mock
     private UsuarioRepository userRepository;
 
     @Test
     public void obtenerUsuarioByUsernameTest(){
+        Usuario usuario = new Usuario();
+        
+        usuario.setId(null);
+        usuario.setNombre("Gabriel");
+        usuario.setApellido("Garrido");
+        usuario.setUsername("leirbaG");
+        usuario.setPassword("Pass1010");
 
-        Usuario userbyUsername = userRepository.findByUsername("admin");
 
-        assertNotNull(userbyUsername.getId());
-        assertEquals(userbyUsername.getUsername(), "admin");
+        when(userRepository.findByUsername("leirbaG")).thenReturn(usuario);
+
+        Usuario userbyUsername = userRepository.findByUsername("leirbaG");
+
+        assertNotNull(userbyUsername);
+        assertEquals(userbyUsername.getUsername(), "leirbaG");
 
     }
 
